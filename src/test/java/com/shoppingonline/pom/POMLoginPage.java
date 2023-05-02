c3586a433241f62971f905825517fcd5270e8855
Sahil Jindal <sahiljindal.thebest@gmail.com>
2023-05-03 02:20:46 +0530

    Updated gitignore

---
+++ src/test/java/com/shoppingonline/pom/POMLoginPage.java
@@ -0,0 +1,29 @@
+package com.shoppingonline.pom;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+public class POMLogin {
+
+    //Login credentials
+    static final String user_email = "otheruser@gmail.com";
+    static final String user_password = "support";
+
+    //Login
+    static final By Login_Email = By.xpath("//*[@id='login']/div[1]/input");
+    static final By Login_Password = By.xpath("//*[@id='login']/div[2]/input");
+    static final By Login_Button = By.xpath("//*[@id='login']/div[4]/div/button");
+
+    WebDriver driver;
+
+    public POMLogin(WebDriver driver) { this.driver = driver; }
+
+    public void login() throws Exception {
+        Thread.sleep(1000);
+        driver.findElement(Login_Email).clear();
+        driver.findElement(Login_Email).sendKeys(user_email);
+        driver.findElement(Login_Password).clear();
+        driver.findElement(Login_Password).sendKeys(user_password);
+        driver.findElement(Login_Button).click();
+    }
+}
---
+++ src/test/java/com/shoppingonline/pom/POMProductsPage.java
@@ -0,0 +1,2 @@
+package com.shoppingonline.pom;public class POMProductsPage {
+}
